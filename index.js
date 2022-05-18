bdeb3a6330256235c3842d13f85ef679ced17489
juanjose_calderon_kairosds <juanjose.calderon@kairosds.com>
2022-05-18 22:33:52 +0200

    feat!: add lit element structure for create a simple web component

--- index.js
+++ index.js
@@ -1,58 +1,65 @@
-import { html, render, LitElement } from 'lit-element';
-import {  } from 'lit-element';
+import { css, html, render, LitElement } from "lit-element";
 
-class AppElement extends HTMLElement {
-    
-    name = this.getAttribute('name') || 'NoName';
+class AppElement extends LitElement {
+  hello = "web0";
 
-    constructor() {
-        super();
-        console.log('inicializando...');
-        this.hello = 'web0';
-        this.attachShadow({ mode: "open" });
-        const template = html`
-            <style>
-                :host {
-                    border: 1px solid white;
-                    display: block;
-                    padding: 1rem;
-                }
-                p{
-                    color: var(--primary-color, blue);
-                }
-                ::slotted(span) {
-                    font-size: 2rem;
-                }
-            </style>
-            <slot></slot>
-            <p part="paragraph">${ this.hello } ${ this.name }</p>
-            <button @click="${ this.clickMe }">Click me!</button>
-        `;
+  // For css the name must to be styles()
+  static get styles() {
+    return css`
+      :host {
+        border: 1px solid white;
+        display: block;
+        padding: 1rem;
+      }
+      p {
+        color: var(--primary-color, blue);
+      }
+      ::slotted(span) {
+        font-size: 2rem;
+      }
+    `;
+  }
 
-        // render(template, document.body, this); If it is not active shadow
-        render(template, this.shadowRoot, this);
-    }
+  // For props the name must to be properties()
+  static get properties() {
+    return {
+      name: { type: String },
+    };
+  }
 
-    clickMe(e) {
-        console.log(e);
-        const message = new CustomEvent("poc:message", {
-            // bubbles is for expand the event for the DOM
-            bubbles: true,
-            detail: {
-                msg: 'Hellow from inside'
-            },
-            /* 
-                The read-only composed property of the Event interface returns a boolean value which indicates 
-                whether or not the event will propagate across the shadow DOM boundary into the standard DOM. 
-            */
-            composed: true,
-        });
-        this.dispatchEvent(message);
-    }
+  constructor() {
+    super();
+    console.log("inicializando...");
+  }
+
+  render() {
+    return html`
+      <slot></slot>
+      <p part="paragraph">${this.hello} ${this.name}</p>
+      <button @click="${this.clickMe}">Click me!</button>
+    `;
+  }
+
+  clickMe(e) {
+    console.log(e);
+    const message = new CustomEvent("poc:message", {
+      // bubbles is for expand the event for the DOM
+      bubbles: true,
+      detail: {
+        msg: "Hellow from inside",
+      },
+      /* 
+        The read-only composed property of the Event interface returns a boolean value which indicates 
+        whether or not the event will propagate across the shadow DOM boundary into the standard DOM. 
+      */
+      composed: true,
+    });
+    this.dispatchEvent(message);
+  }
 }
 
 customElements.define("app-element", AppElement);
 
 customElements.whenDefined("app-element").then(() => {
-    console.log("App element was defined");
-})
+  console.log("App element was defined");
+});
